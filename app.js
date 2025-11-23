c6f59e30ec52d57aa2711ccc7118b316c034df24
Arjun Singh Bhadauriya <arjun22august@gmail.com>
2025-11-23 13:24:21 +0530

    added new functionality

--- app.js
+++ app.js
@@ -88,10 +88,82 @@ function addTodo() {
 const express = require('express');
 const app = express();
 
-app.get('/signup', (req, res) => {
-    res.sendFile(__dirname + "/signup.html");
+let users = [];
+app.use(express.json());
+app.use(express.static("public"));
+
+
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+
+//GET Requests
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
 })
 
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + "/public/sign-up.html");
+})
+
+app.get('/signin', (req, res) => {
+    res.sendFile(__dirname + "/public/sign-in.html");
+})
+
+app.get('/todo', (req, res) => {
+    res.sendFile(__dirname + "/public/todo.html");
+})
+
+
+
+//POST Requests
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(u => u.username == username);
+    if(users.length === 0 || user.username == username) {
+
+        users.push({
+            username: username,
+            password: password
+        });
+        res.send({
+            message : 'You are signed up, Please Sign in'
+        });
+    }
+    else {
+        res.redirect('/signup');
+    }
+})
+
+app.post('signin', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(u => u.username == username && u.password == password);
+
+    if(user) {
+        let token = generateToken();
+        user.token = token;
+        res.send({
+            token: token
+        })
+    }
+    else {
+        res.status(403).send({
+            message: "Invalid Username or Password! Please Try Again."
+        })
+    }
+})
 
 
 app.listen(80);
